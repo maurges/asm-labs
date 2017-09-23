751b95a9b280c801a2a0eba6f71f4b062187d28f
d86leader <d86leader@mail.com>
2017-09-23 11:12:20 +0700

    wrote a sample main file

---
+++ main.asm
@@ -0,0 +1,27 @@
+%include "meta.asm"
+%include "syscalls.asm"
+
+global _start
+
+
+section .text
+
+defun add_vals, a, b
+	add a, b
+	return a
+endfun
+
+_start:
+	mov rax, 5
+	mov rbx, 10
+	vcall add_vals, rax, rbx
+
+	sys_call sys_write, 1, hello_world, hwlength
+	sys_call sys_exit, 0
+
+
+section .data
+
+hello_world:
+	db "hello tupoy pidor", 10
+hwlength equ $ - hello_world
