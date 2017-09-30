1e01a5f6ae3e8062517d8a1c20a51cac394649b1
d86leader <d86leader@mail.com>
2017-09-30 17:07:55 +0700

    completed vigenere

--- vigenere/main.asm
+++ vigenere/main.asm
@@ -75,21 +75,24 @@ endsub
 ; }}}
 
 
+;; accepts one parameter: whether to decrypt the text (1) or encrypt (0)
 ; VigenereCipher {{{
-defsub VigenereCipher
+defun VigenereCipher, decrypt
+	push decrypt ;; [rbp - 8] - decrypt
 	cld
 	;; open files
 
-	defstr src_file, "source.txt"
-	sys_open src_file, FlagR
+	defstr .src_file, "source.txt", 0
+	sys_open .src_file, FlagR
 	mov r15, rax ;; r15 - descriptor of read
 
-	defstr dst_file, "dest.txt"
-	sys_open dst_file, FlagW
+	defstr .dst_file, "dest.txt", 0
+	sys_open .dst_file, FlagW
 	mov r14, rax ;; r14 - descriptor of write
 
 
 	;; get key
+	puts "Enter passphrase:", 10
 	vcall get_pwd, pre_key, FileMaxsize
 	mov r13, rax ;; r13 - size of key
 
@@ -111,7 +114,7 @@ defsub VigenereCipher
 	loop .loop_key
 
 	if r13, e, 0
-	  die "Key should contain at least one letter!"
+	  die "Key should contain at least one letter!", 10
 	endif
   ; }}}
 
@@ -123,6 +126,7 @@ defsub VigenereCipher
 	cmp rax, 0
 	je .loop_end
 
+	mov r11, rax ;; save number of bytes read
 	mov rcx, rax ;; count by number of bytes read
 
 	mov rsi, src_text
@@ -142,11 +146,21 @@ defsub VigenereCipher
 	  ;; get next key symbol
 	  mov bl, [utl_text + r12]
 	  inc r12
-	  ;; cipher al by bl by modulo 26
-	  add al, bl
-	  if al, g, 26
+	  ;; test if should encrypt or decrypt
+	  if [rbp - 8], e, byte 1
+	    ;; decrypting
+	    add al, 26
+	    sub al, bl
+	  else
+	    ;; encrypting
+	    add al, bl
+	  endif
+	  ;; checking if within modulo 26
+	  if al, ge, 26
 	    sub al, 26
 	  endif
+	  ;; restore symbol as letter
+	  call RestoreLetter
 	  ;; write symbol to ciphertext
 	  stosb
 	  ;; check if key index is within bounds
@@ -158,7 +172,7 @@ defsub VigenereCipher
 	;; will loop by rcx set above as number of bytes read
 	loop .loop_chars
 	;; if bytes are done, write them
-	sys_write r14, dst_text, FileMaxsize
+	sys_write r14, dst_text, r11
 	;; and repeat with next chunk
 	jmp .loop_blocks
 	
@@ -168,7 +182,9 @@ defsub VigenereCipher
   .loop_end:
 	sys_close r15
 	sys_close r14
-endsub
+
+	return
+endfun
 ; }}}
 
 ;}}}
