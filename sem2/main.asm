0aed3a30718483ae8c1140544352a3b6db8402d4
d86leader <d86leader@gmail.com>
2018-03-10 14:43:48 +0700

    main now echoes one utterance

--- sem2/main.asm
+++ sem2/main.asm
@@ -1,4 +1,5 @@
 %include "socket.asmh"
+;; vim: ft=nasm
 
 global _start
 
@@ -7,15 +8,58 @@ section .text
 
 ; _start {{{
 _start:
-	call TCPSocketNewBind
+	mov ebp, esp
+	sub esp, 8 ;; two values: two sockfd
 
-	push eax
+	call TCPSocketNewBind
+	mov [ebp-4], eax
+
+	push dword [ebp-4]
 	call TCPSocketAccept
+	mov [ebp-8], eax
+
+	push 0 ;; flags
+	push gotstr_len
+	push gotstr
+	push dword [ebp-8]
+
+	mov eax, 0x66
+	mov ebx, 10 ;; sys_recv
+	mov ecx, esp
+	int 0x80
+
+	push 0 ;; flags
+	push greetstr_len
+	push greetstr
+	push dword [ebp-8] ;; sockfd
+
+	mov eax, 0x66 ;; sys_socketcall
+	mov ebx, 9    ;; sys_send
+	mov ecx, esp
+	int 0x80
+
+	push dword [ebp-8]
+	call TCPSocketClose
+	push dword [ebp-4]
 	call TCPSocketClose
 
 	mov eax, 1
 	mov ebx, 0
 	int 0x80
 ; }}}
+
+; }}}
+
+section .data
+; data {{{
+greetstr: db "go away",10
+greetstr_len equ $ - greetstr
+
+; }}}
+
+section .bss
+; .bss {{{
+gotstr_len equ 1024
+gotstr: resb gotstr_len
 
 ; }}}
